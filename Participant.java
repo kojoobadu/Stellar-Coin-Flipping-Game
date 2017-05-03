4bb005e76f2ebacac3c93ba51d94e734fb793438
kojoobadu <kojoobadu@gmail.com>
2017-05-02 23:25:23 -0500

    Added functionality to check account history and assign an ID to each new participant

--- Participant.java
+++ Participant.java
@@ -8,6 +8,11 @@ import javax.swing.JTextArea;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -55,7 +60,7 @@ public class Participant {
 	 */
 	private void initialize() {
 		setFrame(new JFrame());
-		getFrame().setBounds(100, 100, 792, 448);
+		getFrame().setBounds(100, 100, 792, 416);
 		getFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getFrame().getContentPane().setLayout(null);
 		
@@ -64,7 +69,7 @@ public class Participant {
 		getFrame().getContentPane().add(lblNewLabel);
 		
 		JTextArea displayText = new JTextArea();
-		displayText.setBounds(10, 27, 731, 206);
+		displayText.setBounds(10, 27, 731, 177);
 		frame.getContentPane().add(displayText);
 	
 		
@@ -74,7 +79,7 @@ public class Participant {
 				displayText.setText("Your Account ID is : "+newUser.getAccountId());
 			}
 		});
-		viewAccountIDbtn.setBounds(68, 313, 146, 28);
+		viewAccountIDbtn.setBounds(78, 284, 146, 28);
 		getFrame().getContentPane().add(viewAccountIDbtn);
 		
 		JButton ViewAccountBalancebtn = new JButton("View Account Balance");
@@ -83,17 +88,17 @@ public class Participant {
 				displayText.setText(newUser.querryAccountBalance(newUser.getKeyPair()));
 			}
 		});
-		ViewAccountBalancebtn.setBounds(41, 346, 173, 28);
+		ViewAccountBalancebtn.setBounds(51, 317, 173, 28);
 		getFrame().getContentPane().add(ViewAccountBalancebtn);
 		
 		accountIDTextField = new JTextField();
-		accountIDTextField.setBounds(123, 381, 347, 20);
+		accountIDTextField.setBounds(133, 352, 278, 20);
 		frame.getContentPane().add(accountIDTextField);
 		accountIDTextField.setColumns(10);
 		
 		amountTextField = new JTextField();
 		amountTextField.setColumns(10);
-		amountTextField.setBounds(523, 381, 113, 20);
+		amountTextField.setBounds(478, 346, 113, 20);
 		frame.getContentPane().add(amountTextField);
 		
 		
@@ -109,16 +114,16 @@ public class Participant {
 				}
 			}
 		});
-		TransferFundsbtn.setBounds(643, 370, 123, 28);
+		TransferFundsbtn.setBounds(598, 335, 123, 28);
 		getFrame().getContentPane().add(TransferFundsbtn);
 		
 		JLabel lblEnterYourName = new JLabel("Enter your name and click create account");
 		lblEnterYourName.setHorizontalAlignment(SwingConstants.CENTER);
-		lblEnterYourName.setBounds(10, 244, 256, 14);
+		lblEnterYourName.setBounds(20, 215, 256, 14);
 		frame.getContentPane().add(lblEnterYourName);
 		
 		nameTextField = new JTextField();
-		nameTextField.setBounds(81, 260, 86, 20);
+		nameTextField.setBounds(91, 231, 86, 20);
 		frame.getContentPane().add(nameTextField);
 		nameTextField.setColumns(10);
 		
@@ -130,23 +135,23 @@ public class Participant {
 				nameTextField.setVisible(false);
 			}
 		});
-		createAccountbtn.setBounds(68, 280, 123, 28);
+		createAccountbtn.setBounds(78, 251, 123, 28);
 		frame.getContentPane().add(createAccountbtn);
 		
 		JLabel accountIDText = new JLabel("AccountID to transfer :");
 		accountIDText.setHorizontalAlignment(SwingConstants.CENTER);
-		accountIDText.setBounds(0, 384, 123, 14);
+		accountIDText.setBounds(10, 355, 123, 14);
 		frame.getContentPane().add(accountIDText);
 		
 		JLabel amountText = new JLabel("Amount :");
 		amountText.setHorizontalAlignment(SwingConstants.CENTER);
-		amountText.setBounds(463, 384, 71, 14);
+		amountText.setBounds(418, 349, 71, 14);
 		frame.getContentPane().add(amountText);
 		
 		JLabel lblCoinFlippingGame = new JLabel("Coin Flipping Game");
 		lblCoinFlippingGame.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		lblCoinFlippingGame.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCoinFlippingGame.setBounds(409, 244, 163, 28);
+		lblCoinFlippingGame.setBounds(419, 215, 163, 28);
 		frame.getContentPane().add(lblCoinFlippingGame);
 		
 		JButton btnGenerateRandomNumber = new JButton("Generate Random Number and Hash");
@@ -161,7 +166,7 @@ public class Participant {
 				newUser.updateRandHash(randomN, hashVal, newUser.getAccountId());
 			}
 		});
-		btnGenerateRandomNumber.setBounds(409, 283, 242, 23);
+		btnGenerateRandomNumber.setBounds(419, 254, 242, 23);
 		frame.getContentPane().add(btnGenerateRandomNumber);
 		
 		JButton btnSendHashTo = new JButton("Enter Bankers ID, 20 lumen, and place Bet");
@@ -169,7 +174,7 @@ public class Participant {
 			public void actionPerformed(ActionEvent e) {
 				if(newUser.updateBet()){
 					try {
-						newUser.transferFunds(newUser.getSecretSeed(), accountIDTextField.getText(), amountTextField.getText());
+						newUser.transferFunds(newUser.getSecretSeed() ,accountIDTextField.getText(), amountTextField.getText());
 						displayText.setText("Bet Placed!");
 					} catch (IOException e1) {
 						e1.printStackTrace();
@@ -177,7 +182,7 @@ public class Participant {
 				}
 			}
 		});
-		btnSendHashTo.setBounds(433, 316, 292, 23);
+		btnSendHashTo.setBounds(443, 287, 292, 23);
 		frame.getContentPane().add(btnSendHashTo);
 		
 		JButton bankerIDBtn = new JButton("View Banker Account ID");
@@ -187,8 +192,28 @@ public class Participant {
 				displayText.setText(bankerID);
 			}
 		});
-		bankerIDBtn.setBounds(226, 259, 173, 23);
+		bankerIDBtn.setBounds(211, 230, 173, 23);
 		frame.getContentPane().add(bankerIDBtn);
+		
+		JButton btnViewWinner = new JButton("View Winner");
+		btnViewWinner.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String winner = newUser.getWinner();
+				displayText.setText(winner);
+			}
+		});
+		btnViewWinner.setBounds(271, 272, 123, 23);
+		frame.getContentPane().add(btnViewWinner);
+		
+		JButton historyButton = new JButton("View History");
+		historyButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String history = newUser.viewAccountHistory();
+				displayText.setText(history);
+			}
+		});
+		historyButton.setBounds(271, 307, 138, 23);
+		frame.getContentPane().add(historyButton);
 		
 		
 	}
@@ -207,5 +232,39 @@ public class Participant {
 	
 	public void setUserName(String name){
 		this.userName = name;
+	}
+	
+	public boolean checkParticipant(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception E) {
+			System.err.println("Unable to load driver.");
+			E.printStackTrace();
+		}
+		try {
+			Connection conn1;
+			String dbUrl = "jdbc:mysql://localhost:3306/stellar_data";
+			String user = "root";
+			String password = "root";
+			conn1 = DriverManager.getConnection(dbUrl, user, password);
+			System.out.println("*** Connected to the database ***");
+			Statement statement = conn1.createStatement();
+			ResultSet rs;
+			rs = statement.executeQuery("select * from participants f where f.UserName = 'BANKER'");
+			rs.last();
+			if(rs.getRow() > 0){
+				return true;
+			}
+			statement.close();
+			rs.close();
+			conn1.close();
+		}
+	    catch (SQLException e) {
+	    	System.out.println("SQLException: " + e.getMessage());
+	    	System.out.println("SQLState: " + e.getSQLState());
+	    	System.out.println("VendorError: " + e.getErrorCode());
+	    }
+		
+		return false;
 	}
 }
